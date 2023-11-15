fbf867fb0f0b9dad18f7a99de0daf91641094504
Udhayakumar <udhaya@ecyber.com>
2023-11-15 12:10:22 +0530

    changes added as per E3D team doc

--- src/components/top-section.tsx
+++ src/components/top-section.tsx
@@ -18,8 +18,8 @@ const TopNavTabs: React.FunctionComponent<IMapChangeMode> = (props) => {
 
   const handleTabClick = (label: string) => {
 
-    setActiveTab(label === activeTab ? null : label);
-    // setActiveTab(label);
+    //setActiveTab(label === activeTab ? null : label);
+    setActiveTab(label);
     sendMessage(`You clicked on: ${label}`); 
     
     uxpContext.eventHandler?.( EventsEnum.Navigation, { page: 'map' }); 
@@ -30,26 +30,20 @@ const TopNavTabs: React.FunctionComponent<IMapChangeMode> = (props) => {
     alert(message);
   }
 
-  // const tabs = [
-  //   { label: 'Waste', className: 'nav-link waste', id: 'waste' },
-  //   { label: 'Streetlight', className: 'nav-link streetlight', id: 'streetlight' },
-  //   { label: 'Cctv', className: 'nav-link cctv', id: 'cctv' },
-  //   { label: 'Water', className: 'nav-link water', id: 'water' },
-  //   { label: 'Pollution', className: 'nav-link pollution', id: 'pollution' },
-  //   { label: 'Fire', className: 'nav-link fire', id: 'fire' },
-  // ];
-
-  // const [activeTab, setActiveTab] = useState(null);
-
-  // const handleTabClick = (label) => {
-  //   setActiveTab(label === activeTab ? null : label);
-  // };
+  const tabs = [
+    { label: 'Waste', className: 'nav-link waste', id: 'waste' },
+    { label: 'Streetlight', className: 'nav-link streetlight', id: 'streetlight' },
+    { label: 'Cctv', className: 'nav-link cctv', id: 'cctv' },
+    { label: 'Water', className: 'nav-link water', id: 'water' },
+    { label: 'Pollution', className: 'nav-link pollution', id: 'pollution' },
+    { label: 'Fire', className: 'nav-link fire', id: 'fire' },
+  ]; 
 
   return (
     <WidgetWrapper className="smart-city_box">   
          
           <div className="top-nav-tabs">
-            {/* <ul>
+            <ul>
               {tabs.map((tab) => (
                 <li
                   key={tab.id}
@@ -62,19 +56,12 @@ const TopNavTabs: React.FunctionComponent<IMapChangeMode> = (props) => {
                   </a>
                 </li>
               ))}
-            </ul> */}
+            </ul>
 
 
-          <ul>
-               
-                {/* <li className='nav-link waste '> <a href="#"> <span></span> <label>Waste</label> </a> </li>
-                <li className='nav-link streetlight'> <a href="#"> <span></span> <label>Streetlight</label> </a> </li>
-                <li className='nav-link cctv'> <a href="#"> <span></span> <label>Cctv</label> </a> </li>
-                <li className='nav-link water'> <a href="#"> <span></span> <label>Water</label> </a> </li>
-                <li className='nav-link pollution'> <a href="#"> <span></span> <label>Pollution</label> </a> </li>
-                <li className='nav-link fire'> <a href="#"> <span></span> <label>Fire</label> </a> </li> */}
-
-
+          
+                
+{/* 
 <ul id="navList">
       <li className={`nav-link waste ${activeTab === 'Waste' ? 'active' : ''}`} onClick={() => handleTabClick('Waste')}>
         <a href="#">
@@ -112,9 +99,9 @@ const TopNavTabs: React.FunctionComponent<IMapChangeMode> = (props) => {
           <label>Fire</label>
         </a>
       </li>
-    </ul>
+    </ul> */}
               
-            </ul>
+           
 
 
           </div>
